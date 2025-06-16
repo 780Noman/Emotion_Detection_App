99df9c5bebe56280d172a34fd31c1d1773750dad
Noman Amjad <nq6145841@gmail.com>
2025-06-16 08:31:45 +0500

    Fix: Added STATIC_ROOT to settings for deployment

--- FER_deploy/settings.py
+++ FER_deploy/settings.py
@@ -117,9 +117,12 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.1/howto/static-files/
+# https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+# The absolute path to the directory where collectstatic will collect static files for deployment.
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
